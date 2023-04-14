c60fabfb0dd8143e5777f2eebb306f24aa1088b5
JuliaHoladava <juliaholadava@gmail.com>
2023-04-14 20:32:58 +0200

    feat: add functions for Count cats, Carbon-dating, Dream team, What season, Tower of Hanoi

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let nameTeam = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      let nameMember = members[i].trim().toUpperCase();
+      nameTeam += nameMember.slice(0, 1);
+    }
+  }
+  nameTeam = nameTeam.split('').sort().join('');
+  if (nameTeam.length > 0) {
+    return nameTeam;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
