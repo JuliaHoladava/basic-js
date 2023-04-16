1003de4289a0f47a27962cfaa357beb116af2088
JuliaHoladava <juliaholadava@gmail.com>
2023-04-16 14:29:01 +0200

    feat: add functions for Recursive depth calculator, Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator }) {//{ repeatTimes = 1, separator = '+', addition, additionRepeatTimes = 1, additionSeparator = '|' }) {
+  repeatTimes = typeof repeatTimes !== 'undefined' ?  repeatTimes : 1;
+  separator = typeof separator !== 'undefined' ?  separator : '+';
+  addition = typeof addition !== 'undefined' ?  addition : '';
+  additionRepeatTimes = typeof additionRepeatTimes !== 'undefined' ?  additionRepeatTimes : 1;
+  additionSeparator = typeof additionSeparator !== 'undefined' ?  additionSeparator : '|';
+
+  let repeatStr = '';
+  str = String(str);
+  addition = String(addition);
+  for (let i = 0; i < repeatTimes; i++) {
+    repeatStr += str;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      repeatStr += addition;
+      if (j <additionRepeatTimes - 1) {
+        repeatStr += additionSeparator;
+      }
+    }
+    if (i < repeatTimes - 1) {
+      repeatStr += separator;
+    }
+  }
+  return repeatStr;
 }
+
 
 module.exports = {
   repeater
